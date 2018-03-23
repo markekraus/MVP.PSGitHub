36432501ad31c11d58b88857a39eeadf843d84f1
Mark Kraus <github@markekraus.com>
2018-03-23 12:56:02 -0500

    Add New-MVPPowerShellRfcComment

---
+++ src/MVP.PSGitHub/Public/New-MVPPowerShellRfcComment.ps1
@@ -0,0 +1,32 @@
+function New-MVPPowerShellRfcComment {
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        [datetime]
+        $StartDate = [datetime]::Now,
+
+        [long]
+        $RfcPRNumber
+    )
+    process {
+        $PR = Invoke-RestMethod -Uri "https://api.github.com/repos/PowerShell/PowerShell-RFC/pulls/$RfcPRNumber" -ErrorAction stop
+        if(-not $PR) {
+            Write-Error "PowerShell/PowerShell-RFC#$RfcPRNumber not found."
+            return
+        }
+        $Params = @{
+            StartDate              = $StartDate.ToString('yyy-MM-dd')
+            Title                  = 'PowerShell RFC Comment - {0} #{1}' -f $PR.title, $RfcPRNumber
+            Description            = 'Provide feedback on PowerShell RFC' 
+            ReferenceUrl           = $PR.html_url 
+            AnnualQuantity         = 1 
+            SecondAnnualQuantity   = 0 
+            AnnualReach            = 0 
+            Visibility             = 'Everyone'
+            ContributionTechnology = 'PowerShell'
+            ContributionType       = 'Open Source Project(s)'
+        }
+        if ($PSCmdlet.ShouldProcess($($Params | ConvertTo-Json -Compress))) {
+            New-MVPContribution @Params
+        }
+    }
+}
