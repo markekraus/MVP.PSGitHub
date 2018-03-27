add680dcde2cd1fd887aef8af915a1a280810bf4
Mark Kraus <github@markekraus.com>
2018-03-27 04:20:52 -0500

    Add New-MVPPowerShellDocumentationContribution

---
+++ src/MVP.PSGitHub/Public/New-MVPPowerShellDocumentationContribution.ps1
@@ -0,0 +1,39 @@
+function New-MVPPowerShellDocumentationContribution {
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        [datetime]
+        $StartDate = [datetime]::Now,
+
+        [String]
+        $Owner = 'PowerShell',
+
+        [String]
+        $Project = 'PowerShell-Docs',
+
+        [Parameter(Mandatory)]
+        [long]
+        $PRNumber
+    )
+    process {
+        $PR = Invoke-RestMethod -Uri "https://api.github.com/repos/$Owner/$Project/pulls/$PRNumber" -ErrorAction stop
+        if(-not $PR) {
+            Write-Error "$Owner/$Project#$PRNumber not found."
+            return
+        }
+        $Params = @{
+            StartDate              = $StartDate.ToString('yyy-MM-dd')
+            Title                  = 'PowerShell Documentation - {0} #{1}' -f $PR.title, $PRNumber
+            Description            = 'Provide documentation for PowerShell.' 
+            ReferenceUrl           = $PR.html_url 
+            AnnualQuantity         = 1 
+            SecondAnnualQuantity   = 0 
+            AnnualReach            = 0 
+            Visibility             = 'Everyone'
+            ContributionTechnology = 'PowerShell'
+            ContributionType       = 'Open Source Project(s)'
+        }
+        if ($PSCmdlet.ShouldProcess($($Params | ConvertTo-Json -Compress))) {
+            New-MVPContribution @Params
+        }
+    }
+}
