be9e1a74c946462f4afd83b081fb9c6fa23deae1
Mark Kraus <github@markekraus.com>
2018-03-29 07:47:50 -0500

    Add owner and project to New-MVPPowerShellIssueCommentContribution

--- Docs/New-MVPPowerShellIssueCommentContribution.md
+++ Docs/New-MVPPowerShellIssueCommentContribution.md
@@ -13,8 +13,8 @@ schema: 2.0.0
 ## SYNTAX
 
 ```
-New-MVPPowerShellIssueCommentContribution [[-StartDate] <DateTime>] [[-IssueNumber] <Int64>] [-WhatIf]
- [-Confirm] [<CommonParameters>]
+New-MVPPowerShellIssueCommentContribution [[-StartDate] <DateTime>] [-Owner <String>] [-Project <String>]
+ [[-IssueNumber] <Int64>] [-WhatIf] [-Confirm] [<CommonParameters>]
 ```
 
 ## DESCRIPTION
@@ -41,6 +41,36 @@ Aliases:
 
 Required: False
 Position: 1
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -Owner
+{{Fill Owner Description}}
+
+```yaml
+Type: String
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -Project
+{{Fill Project Description}}
+
+```yaml
+Type: String
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: Named
 Default value: None
 Accept pipeline input: False
 Accept wildcard characters: False
--- src/MVP.PSGitHub/Public/New-MVPPowerShellIssueCommentContribution.ps1
+++ src/MVP.PSGitHub/Public/New-MVPPowerShellIssueCommentContribution.ps1
@@ -3,20 +3,25 @@ function New-MVPPowerShellIssueCommentContribution {
     param (
         [datetime]
         $StartDate = [datetime]::Now,
+        [String]
+        $Owner = 'PowerShell',
+
+        [String]
+        $Project = 'PowerShell',
 
         [long]
         $IssueNumber
     )
     process {
-        $PR = Invoke-RestMethod -Uri "https://api.github.com/repos/PowerShell/PowerShell/issues/$IssueNumber" -ErrorAction stop
+        $PR = Invoke-RestMethod -Uri "https://api.github.com/repos/$Owner/$Project/issues/$IssueNumber" -ErrorAction stop
         if(-not $PR) {
             Write-Error "PowerShell/PowerShell#$IssueNumber not found."
             return
         }
         $Params = @{
             StartDate              = $StartDate.ToString('yyy-MM-dd')
-            Title                  = 'PowerShell Issue Comment - {0} #{1}' -f $PR.title, $RfcPRNumber
-            Description            = 'Provide feedback on PowerShell Issue' 
+            Title                  = 'PowerShell Issue Comment - {0} #{1}' -f $PR.title, $IssueNumber
+            Description            = 'Provide feedback on PowerShell Issue.'
             ReferenceUrl           = $PR.html_url 
             AnnualQuantity         = 1 
             SecondAnnualQuantity   = 0 
